bfc11142ef2131f965f5ada141a56dd0590139ba
Angelica <Angelica137@users.noreply.github.com>
2022-06-04 18:13:27 +0100

    delete obsolete

--- scripts/leap.py
+++ scripts/leap.py
@@ -1,9 +1,2 @@
 def leap(year):
     return "leap 🐰" if (year % 400 == 0) or ((year % 4 == 0) and (year % 100 != 0)) else "not leap 📆"
-    if year % 400 == 0:
-        return "leap 🐰"
-    elif year % 100 == 0:
-        return "not leap 📆"
-    elif year % 4 == 0:
-        return "leap 🐰"
-    return "not leap 📆"
